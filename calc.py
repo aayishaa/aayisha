82d0c5218ff729d3bfbbef0bbd3f12a20c3cff2e
aayishaa <42862972+aayishaa@users.noreply.github.com>
2019-02-21 14:32:18 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,39 @@
+a=20
+b=10
+c=0
+c=a+b
+print(c)
+c=a-b
+print(c) 
+c = a * b
+print(c) 
+c = a / b
+print(c) 
+c = a % b
+print(c)
+c = a**b 
+print(c)
+c = a//b 
+print(c)
+c=a<<b
+print(c)
+c=a>>b
+print(c)
+c=(a==b)
+print(c)
+c=(a!=b)
+print(c)
+c=(a>=b)
+print(c)
+c=(a<=b)
+print(c)
+c=(a<b)
+print(c)	
+c=(a&b)
+print(c)
+c=(a|b)
+print(c)
+c=a is b
+print (c)
+c=a is not b
+print(c)
